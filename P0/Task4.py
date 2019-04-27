6e0f5c96985d6f80d78d72ffe7bebce74e6245e9
Claudio Rodriguez <claudio.rodriguez@gatherhere.com>
2019-04-26 21:09:19 -0500

    feat: task 4

--- P0/Task4.py
+++ P0/Task4.py
@@ -25,3 +25,15 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+makes_calls = set()
+receives_calls_sends_texts_receives_texts = set()
+for record in calls:
+  makes_calls.add(record[0])
+  receives_calls_sends_texts_receives_texts.add(record[1])
+for record in texts:
+  receives_calls_sends_texts_receives_texts.add(record[0])
+  receives_calls_sends_texts_receives_texts.add(record[1])
+
+print("These numbers could be telemarketers: ")
+possible_telemarketers = sorted(makes_calls.difference(receives_calls_sends_texts_receives_texts))
+print("\n".join(possible_telemarketers))
