2b8c375bc41d02f2e9649193fa76cc37830cbdba
claudiordgz <claudio.rdgz@gmail.com>
2019-06-11 00:05:45 -0400

    trie problems

---
+++ P2/problem_5.py
@@ -0,0 +1,103 @@
+# # Building a Trie in Python
+# 
+# Before we start let us reiterate the key components of a Trie or Prefix Tree. A trie is a tree-like data structure that stores a dynamic set of strings. Tries are commonly used to facilitate operations like predictive text or autocomplete features on mobile phones or web search.
+# 
+# Before we move into the autocomplete function we need to create a working trie for storing strings.  We will create two classes:
+# * A `Trie` class that contains the root node (empty string)
+# * A `TrieNode` class that exposes the general functionality of the Trie, like inserting a word or finding the node which represents a prefix.
+# Give it a try by implementing the `TrieNode` and `Trie` classes below!
+from collections import defaultdict
+
+
+class TrieNode:
+    def __init__(self):
+        self.is_word = False
+        self.children = defaultdict(TrieNode)
+
+    def insert(self, char):
+        # Add a child node in this Trie
+        return self.children[char]
+
+    def has_children(self, char):
+        return char in self.children
+
+    def get_child(self, char):
+        return self.children[char] if char in self.children else None
+
+    def set_word(self):
+        self.is_word = True
+
+    def _suffixes(self, node, s_list, suffix=''):
+        for char in node.children:
+            s = "{}{}".format(suffix, char)
+            self._suffixes(node.get_child(char), s_list, s)
+        if node.is_word:
+            s_list.append(suffix)
+
+    def suffixes(self):
+        # Recursive function that collects the suffix for 
+        # all complete words below this point
+        s = []
+        self._suffixes(self, s)
+        return s
+
+
+# The Trie itself containing the root node and insert/find functions
+class Trie:
+    def __init__(self):
+        # Initialize this Trie (add a root node)
+        self.root = TrieNode()
+
+    def insert(self, word):
+        # Add a word to the Trie
+        itr = self.root
+        for char in word:
+            itr = itr.insert(char)
+        itr.set_word()
+
+    def find(self, prefix):
+        # Find the Trie node that represents this prefix
+        itr = self.root
+        for char in prefix:
+            itr = itr.get_child(char)
+        return itr
+
+
+MyTrie = Trie()
+wordList = [
+    "ant", "anthology", "antagonist", "antonym", 
+    "fun", "function", "factory", 
+    "trie", "trigger", "trigonometry", "tripod"
+]
+for word in wordList:
+    MyTrie.insert(word)
+
+
+def test_case(trie):
+    node = trie.find("a")
+    suffixes = node.suffixes()
+    print('is correct for a? ', suffixes == ['nthology', 'ntagonist', 'ntonym', 'nt'], suffixes)
+    node = trie.find('f')
+    suffixes = node.suffixes()
+    print('is correct for f? ', suffixes == ['unction','un','actory'], suffixes)
+
+
+def test_case_longer(trie):
+    node = trie.find("tri")
+    suffixes = node.suffixes()
+    print('is correct for tri? ', suffixes == ['e', 'gger', 'gonometry', 'pod'], suffixes)
+    node = trie.find('ant')
+    suffixes = node.suffixes()
+    print('is correct for ant? ', suffixes == ['hology', 'agonist', 'onym', ''], suffixes)
+
+
+def test_case_invalid(trie):
+    node = trie.find("c")
+    print('is c None? ', node is None)
+
+
+test_case(MyTrie)
+
+test_case_longer(MyTrie)
+
+test_case_invalid(MyTrie)
--- P2/problem_7.py
+++ P2/problem_7.py
@@ -1,60 +1,134 @@
+from collections import defaultdict
+
+
+class RouteTrieNode:
+
+    def __init__(self):
+        # Initialize the node with children as before, plus a handler
+        self.handler = None
+        self.children = defaultdict(RouteTrieNode)
+
+    def set_handler(self, handler):
+        self.handler = handler
+
+    def get_handler(self):
+        return self.handler
+
+
 # A RouteTrie will store our routes and their associated handlers
 class RouteTrie:
-    def __init__(self, ...):
-        # Initialize the trie with an root node and a handler, this is the root path or home page node
 
-    def insert(self, ...):
+    def __init__(self, handler):
+        # Initialize the trie with an root node and a handler, this is the root path or home page node
+        self.root = RouteTrieNode()
+        self.root.set_handler(handler)
+
+    def insert(self, list_of_words, handler):
         # Similar to our previous example you will want to recursively add nodes
         # Make sure you assign the handler to only the leaf (deepest) node of this path
+        itr = self.root
+        for word in list_of_words:
+            itr = itr.children[word]
+        itr.set_handler(handler)
 
-    def find(self, ...):
+    def find(self, list_of_words):
         # Starting at the root, navigate the Trie to find a match for this path
         # Return the handler for a match, or None for no match
+        itr = self.root
+        for word in list_of_words:
+            if word not in itr.children:
+                return None
+            itr = itr.children[word]
+        return itr.get_handler()
 
+    def get_root_handler(self):
+        return self.root.get_handler()
 
-# A RouteTrieNode will be similar to our autocomplete TrieNode... with one additional element, a handler.
-class RouteTrieNode:
-    def __init__(self, ...):
-        # Initialize the node with children as before, plus a handler
+    def _iterate(self, node, lists, c_list=[]):
+        for word in node.children:
+            c = c_list + [word]
+            self._iterate(node.children[word], lists, c)
 
-    def insert(self, ...):
-        # Insert the node as before
+        if node.get_handler() is not None:
+            lists.append(c_list)
+
+    def get_all_lists_of_words(self):
+        itr = self.root
+        lists = []
+        self._iterate(itr, lists)
+        return lists
 
 
 # The Router class will wrap the Trie and handle 
 class Router:
-    def __init__(self, ...):
+    def __init__(self, root_handler, _404_handler):
         # Create a new RouteTrie for holding our routes
         # You could also add a handler for 404 page not found responses as well!
+        self.trie = RouteTrie(root_handler)
+        self.error_handler = _404_handler
 
-    def add_handler(self, ...):
+    def add_handler(self, path, handler):
         # Add a handler for a path
         # You will need to split the path and pass the pass parts
         # as a list to the RouteTrie
+        parts = self.split_path(path)
+        self.trie.insert(parts, handler)
 
-    def lookup(self, ...):
+    def lookup(self, path):
         # lookup path (by parts) and return the associated handler
         # you can return None if it's not found or
         # return the "not found" handler if you added one
         # bonus points if a path works with and without a trailing slash
         # e.g. /about and /about/ both return the /about handler
+        if path == "/":
+            return self.trie.get_root_handler()
 
+        parts = self.split_path(path)
+        handler = self.trie.find(parts)
+        if handler is None:
+            return self.error_handler
+        return handler
 
-    def split_path(self, ...):
+    def split_path(self, path):
         # you need to split the path into parts for 
-        # both the add_handler and loopup functions,
+        # both the add_handler and lookup functions,
         # so it should be placed in a function here
+        _path = path if path[-1] != "/" else path[:-1]
+        return _path.split("/")
+
+    def get_sitemap(self):
+        list_paths = self.trie.get_all_lists_of_words()
+        paths = ["/".join(l) if l else "/" for l in list_paths]
+        return paths
 
 
-# Here are some test cases and expected outputs you can use to test your implementation
+def test_cases():
+    # Here are some test cases and expected outputs you can use to test your implementation
+    # create the router and add a route
+    router = Router("root handler", "not found handler") # remove the 'not found handler' if you did not implement this
+    router.add_handler("/home/about", "about handler")  # add a route
 
-# create the router and add a route
-router = Router("root handler", "not found handler") # remove the 'not found handler' if you did not implement this
-router.add_handler("/home/about", "about handler")  # add a route
+    # some lookups with the expected output
+    handler = router.lookup("/")
+    print(handler, ', is correct?', handler == 'root handler')        # should print 'root handler'
+    handler = router.lookup("/home")
+    print(handler, ', is correct?', handler == 'not found handler')   # should print 'not found handler' or None if you did not implement one
+    handler = router.lookup("/home/about")
+    print(handler, ', is correct?', handler == 'about handler')       # should print 'about handler'
+    handler = router.lookup("/home/about/")
+    print(handler, ', is correct?', handler == 'about handler')       # should print 'about handler' or None if you did not handle trailing slashes
+    handler = router.lookup("/home/about/me")
+    print(handler, ', is correct?', handler == 'not found handler')   # should print 'not found handler' or None if you did not implemen
 
-# some lookups with the expected output
-print(router.lookup("/")) # should print 'root handler'
-print(router.lookup("/home")) # should print 'not found handler' or None if you did not implement one
-print(router.lookup("/home/about")) # should print 'about handler'
-print(router.lookup("/home/about/")) # should print 'about handler' or None if you did not handle trailing slashes
-print(router.lookup("/home/about/me")) # should print 'not found handler' or None if you did not implemen
+    router.add_handler("/blog/2019-01-15/my-awesome-blog-post", "awesome blog post content")
+    l = sorted(router.get_sitemap())
+    print(l, ' is correct?', l == ['/', '/blog/2019-01-15/my-awesome-blog-post', '/home/about'])
+
+
+test_cases()
+# root handler , is correct? True
+# not found handler , is correct? True
+# about handler , is correct? True
+# about handler , is correct? True
+# not found handler , is correct? True
+# ['/', '/blog/2019-01-15/my-awesome-blog-post', '/home/about']  is correct? True
