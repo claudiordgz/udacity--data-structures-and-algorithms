65b9884b701f250ce1fca7cad79a0cc2981b2cd6
claudiordgz <claudio.rdgz@gmail.com>
2019-06-13 23:27:38 -0400

    problem 6

--- P2/explanation_6.md
+++ P2/explanation_6.md
@@ -5,3 +5,9 @@ In this problem, we will look for smallest and largest integer from a list of un
 Bonus Challenge: Is it possible to find the max and min in a single traversal?
 
 Sorting usually requires O(n log n) time Can you come up with a O(n) algorithm (i.e., linear time)?
+
+# Solution
+
+Searching for min and max in an unsorted array can be done in `O(n)` time. To do it, we keep track of two variables, min, and max, and we set max to a minimum value and min to a massive one. 
+
+We traverse the array one time, checking each number to see if it's larger than max, or smaller than min. 
--- P2/problem_6.py
+++ P2/problem_6.py
@@ -1,5 +1,5 @@
 import random
-
+import sys
 
 def get_min_max(ints):
     """
@@ -8,14 +8,36 @@ def get_min_max(ints):
     Args:
        ints(list): list of integers containing one or more integers
     """
+    _max = -sys.maxsize -1
+    _min = sys.maxsize
 
     for i in ints:
-        pass
+        if i > _max:
+            _max = i
+        if i < _min:
+            _min = i
+    
+    return (_min, _max)
 
 
 # Example Test Case of Ten Integers
 
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
-
 print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+# Pass
+
+l = [i for i in range(0, 2)]
+random.shuffle(l)
+print("Pass" if ((0, 1) == get_min_max(l)) else "Fail")
+# Pass
+
+l = [i for i in range(10, 1512)]
+random.shuffle(l)
+print("Pass" if ((10, 1511) == get_min_max(l)) else "Fail")
+# Pass
+
+l = [i for i in range(-31551351, 106489716)]
+random.shuffle(l)
+print("Pass" if ((-31551351, 106489715) == get_min_max(l)) else "Fail")
+# Pass
